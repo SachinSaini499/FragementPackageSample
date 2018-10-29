60711ed24667b83e0563e84c625ec83db34ce78c
sachinsaini499 <sachinsaini499@gmail.com>
2018-10-30 01:00:06 +0530

    FragementSample

---
+++ Dimensions/Resources/fragment/MainMenuFragment.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+using System;
+
+namespace Dimensions.Resources.fragment
+{
+    public class MainMenuFragment : Fragment
+    {
+        View view;
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);            
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreateView(inflater, container, savedInstanceState);
+                 view = inflater.Inflate(Resource.Layout.MainMenuFragment, container, false);
+                Button addDimensionButton = view.FindViewById<Button>(Resource.Id.addDimensionBtn);
+                addDimensionButton.Click += AddDimensionBtnClicked;
+                Button showDimensionButton = view.FindViewById<Button>(Resource.Id.showDimensionBtn);
+                showDimensionButton.Click += ShowDimensionBtnClicked;
+            }
+            catch (Exception ex) { }
+            return view;
+        }
+
+        private void AddDimensionBtnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Fragment addDetails = new AddDetailsFragment();
+                FragmentTransaction fragmentTransaction = FragmentManager.BeginTransaction();
+                fragmentTransaction.SetTransition(FragmentTransit.EnterMask);
+                fragmentTransaction.Replace(Resource.Id.fragment_container, addDetails);
+                fragmentTransaction.Commit();
+            }
+            catch (Exception ex) { }
+        }
+
+        private void ShowDimensionBtnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Fragment packageDetails = new PackageDetailsFragment();
+                FragmentTransaction fragmentTransaction = FragmentManager.BeginTransaction();
+                fragmentTransaction.SetTransition(FragmentTransit.EnterMask);
+                fragmentTransaction.Replace(Resource.Id.fragment_container, packageDetails);
+                fragmentTransaction.Commit();
+            }
+            catch (Exception ex) { }
+
+        }
+    }
+}
---
+++ Dimensions/Resources/fragment/PackageDetailsFragment.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Dimensions.CustomControl;
+using Dimensions.Database;
+using Dimensions.Database.Tables;
+
+namespace Dimensions.Resources.fragment
+{
+    public class PackageDetailsFragment : Fragment
+    {
+        View view;
+        SQLiteDB<BarcodeDetailsTbl> sQLiteDB;
+        List<BarcodeDetailsTbl> lstBarcodeDetails;
+        private ListView pckgListView;
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            try
+            {
+                base.OnCreate(savedInstanceState);
+                sQLiteDB = new SQLiteDB<BarcodeDetailsTbl>();
+            }
+            catch(Exception ex) { }
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            try
+            {
+
+                base.OnCreateView(inflater, container, savedInstanceState);
+                view = inflater.Inflate(Resource.Layout.PackageDetailsFragment, container, false);
+                lstBarcodeDetails = sQLiteDB.GetData().ToList<BarcodeDetailsTbl>();
+                if (lstBarcodeDetails != null)
+                {
+                    pckgListView = view.FindViewById<ListView>(Resource.Id.lstViewPackageDetail);
+                    pckgListView.Adapter = new CusotmListAdapter(this.Activity, lstBarcodeDetails);
+                }
+                else
+                {
+                    (Activity as MainActivity).textToast("No package found!");
+                }
+            }
+            catch (Exception ex) { }
+            return view;
+        }
+    }
+}
